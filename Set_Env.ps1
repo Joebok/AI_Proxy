013173df64c8a1dc76f1837df9510483674b97da
Joe Schonbok <joebok@gmail.com>
2026-07-29 05:55:48 -0700

    Working with Checkpoint Lab

---
+++ Set_Env.ps1
@@ -0,0 +1,2 @@
+[System.Environment]::SetEnvironmentVariable("ZET_PROJECT_ROOT", "C:\Users\Joe\Projects\Zet", "User")
+[System.Environment]::SetEnvironmentVariable("CHECKPOINT_LAB_PROJECT_ROOT", "C:\Users\Joe\Projects\CheckpointLab", "User")
